a268e409e39082908ae55021e11d6e4ddd3057c8
Denis <den748@gmail.com>
2023-01-26 13:37:42 +0300

    release ze kraken

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Card from './Card';
+import Card from './Card/Card';
 import Header from './Header';
+// import '../styles/styles.css';
 
 export default function App() {
   return (
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-let vId;
+
 export default function Card() {
   const { id } = useParams();
   const [item, setItem] = useState({});
   // if (id !== '') {
-  axios('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52767')
-    .then((res) => setItem(res.data.meals[0]));
+  useEffect(() => {
+    try {
+      axios(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52767`)
+        .then((res) => setItem(res.data.meals[0]));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   // }
   const keys = Object.keys(item);
   const vals = Object.values(item);
@@ -19,25 +25,34 @@ export default function Card() {
   const measuresRegexp = /strMeasure\d\d?/gm;
   const ingrNamesArr = keys.filter((el) => el.match(ingrRegexp));
   const measuresArr = keys.filter((el) => el.match(measuresRegexp));
-  // const ourUrl = item.strYoutube;
-  // // console.log(ourUrl);
-  // const str = ourUrl.split('=');
-  // vId = str[str.length - 1];
+  let vId = '';
+  if (item.strYoutube) {
+    const ourUrl = item.strYoutube;
+    // console.log(ourUrl);
+    const str = ourUrl.split('=');
+    vId = str[str.length - 1];
+  }
+  const clickHandler = () => {
+
+  };
   return (
     <>
       <div className="content">
-        <img src={item.strMealThumb} alt="" />
         <div className="innerContent">
-          <h1>{item.strMeal}</h1>
-          <h2>
+          <img src={item.strMealThumb} alt="" />
+          <div className="dish-btn">
+            <h1>{item.strMeal}</h1>
+            <button className="btn" type="button" onClick={clickHandler}>Add to favourites</button>
+          </div>
+          <h4>
             {item.strArea}
             {' '}
             Cuisine
-          </h2>
-          <h3>
+          </h4>
+          <h4>
             Category:
             {item.strCategory}
-          </h3>
+          </h4>
 
         </div>
       </div>
@@ -63,7 +78,8 @@ export default function Card() {
           </h5>
         </div>
         <div className="video">
-          <iframe src={`https://www.youtube.com/embed/1ahpSTf_Pvk`} />
+
+          <iframe src={`https://www.youtube.com/embed/${vId}`} />
         </div>
       </div>
     </>
